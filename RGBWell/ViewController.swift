4905d0ef7be8fa2e693e815c33230cd1a8dcb867
Tara Singh M C <tarasing@ceino.com>
2018-07-08 22:16:07 +0530

    first commit

---
+++ RGBWell/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RGBWell
+//
+//  Created by Tarasing Muchhal Chandrashekharnaik on 29/06/18.
+//  Copyright © 2018 Exilant. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
